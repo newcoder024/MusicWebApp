2ddd46c55665d015c98ef99daaead3dc0bde1417
Julia Heemskerk <juliaheemskerk@icloud.com>
2025-06-01 22:18:59 +0200

    start
    search bar works, added icons for home and playlists and put the search bar in the center.

---
+++ app.js
@@ -0,0 +1,63 @@
+function searchMusic(){
+    const input = document.getElementById('searchInput').value;
+    const results = document.getElementById('results');
+    const loading = document.getElementById('loading');
+
+    results.innerHTML = '';
+    loading.textContent = 'Searching...'; // laat zoek-tekst zien
+
+    if (!input){ // als er geen input is
+        loading.textContent = 'Please enter a search term.';
+        return;
+    }
+
+    // API-aanroep
+    fetch(`https://itunes.apple.com/search?term=${encodeURIComponent(input)}&media=music&limit=12`)
+    .then(response => response.json()) // verwerk de JSON-respons
+    .then(data => { // verwerk de data
+        loading.textContent = ''; // verwijder zoek-tekst
+        if (data.results.length === 0) { // als er geen resultaten zijn
+            results.innerHTML = '<p>No results found.</p>';
+            return;
+        }
+
+        data.results.forEach(song => { // loop door de resultaten
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.innerHTML = `
+            <img src="${song.artworkUrl100.replace('100x100', '400x400')}" alt="${song.trackName}">
+            <h3 class="songTitle">${song.trackName}</h3>
+            <p class="artist" data-artist="${song.artistName}">${song.artistName}</p>
+            <p>Album: ${song.collectionName}</p>
+            <audio controls>
+                <source src="${song.previewUrl}" type="audio/mpeg">
+                Your browser does not support the audio element.
+            </audio>
+            `;
+
+            results.appendChild(card);
+        })
+
+        // laat meer van zelfde artiest zien..
+        document.querySelectorAll('.artist').forEach(artistEl => {
+            artistEl.addEventListener('click', () => {
+                
+                const artist = artistEl.getAttribute('data-artist');
+                searchMusic(artist);
+            })
+        })
+
+
+    })
+    .catch(err => {
+        console.error(err);
+        loading.textContent = 'An error occurred while fetching data.';
+    })
+}
+
+// Event listener voor de zoekknop
+document.getElementById('searchInput').addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        searchMusic();
+    }
+})
